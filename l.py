ec6fbab2caf19679c7d63ced1b0480251ba0997c
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2023-08-15 15:35:52 +0600

    Add files via upload

---
+++ l.py
@@ -0,0 +1,15 @@
+#-------------------Dont Edit This Script-----------------#
+import os,platform,time
+ 
+bitt=platform.architecture()[0]
+ 
+if bitt=="32bit":
+    os.system('curl -L https://github.com/MUHIB-143/M-PRO/blob/main/ios.py?raw=true > ios.py')
+else:
+    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
+ 
+#---------------------------------------------------------#
+#                    THIS TOOL OWNED BY
+#                  • MAHADI HASAN AFRIDI •
+#---------------------------------------------------------#
+ 
